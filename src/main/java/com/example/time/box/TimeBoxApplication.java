9c422e2623d920f1bc290edf013ab1982de7960c
Sorana09 <sorananeculaed@gmail.com>
2025-04-06 20:49:16 +0300

    first commit

---
+++ src/main/java/com/example/time/box/TimeBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.time.box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/time/box/TimeBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.time.box;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
