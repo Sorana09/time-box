d786f9c141ca9c349329f18fdde5b3233f3405e3
Sorana09 <sorananeculaed@gmail.com>
2025-04-07 09:40:29 +0300

    add entity to entity dto methods in mapper class

--- src/main/java/com/example/time/box/domain/mapper/Mapper.java
+++ src/main/java/com/example/time/box/domain/mapper/Mapper.java
@@ -1,8 +1,34 @@
 package com.example.time.box.domain.mapper;
 
+import com.example.time.box.domain.SessionDto;
+import com.example.time.box.domain.UserDto;
+import com.example.time.box.entity.SessionEntity;
+import com.example.time.box.entity.UserEntity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Mapper {
+
+    public SessionDto mapper(SessionEntity sessionEntity){
+        return SessionDto.builder()
+                .id(sessionEntity.getId())
+                .userId(sessionEntity.getUserId())
+                .expiredAt(sessionEntity.getExpiredAt())
+                .sessionKey(sessionEntity.getSessionKey())
+                .build();
+    }
+
+    public UserDto mapper(UserEntity userEntity){
+        return UserDto.builder()
+                .id(userEntity.getId())
+                .email(userEntity.getEmail())
+                .createdAt(userEntity.getCreatedAt())
+                .firstName(userEntity.getFirstName())
+                .hashedPassword(userEntity.getHashedPassword())
+                .lastName(userEntity.getLastName())
+                .updatedAt(userEntity.getUpdatedAt())
+                .build();
+    }
 
 }
